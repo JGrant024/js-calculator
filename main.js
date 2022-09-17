825de229ae5a664566db5533c1f92cb55d401efa
Jonathan Grant <jonathangrant@Jonathans-MacBook-Air.local>
2022-09-17 11:50:54 -0400

    updated js

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
+class Calculator {
+     constructor(previousOperandTexetElement, currentOperandTexetElement){
+        this.previousOperandTexetElement = previousOperandTexetElement
+        this,currentOperandTexetElement = currentOperandTexetElement
+        this.clear  
+     }
+}
+clear() {
+
+}
+
+delete(){
+
+}
+ 
+appendNumber(number) {
+
+}
+
+chooseOperation(operation) {
+
+}
+
+compute() {
+
+}
+
+updateDisplay() {
+    this.currentOperandTexetElement.innerText 
+}
+
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButtons = document.querySelectorAll('[data-equals]')
@@ -5,3 +37,12 @@ const deleteButton = document.querySelectorAll('[data-delete]')
 const allClearButton = document.querySelectorAll('[data-all-clear]')
 const previousOperandTexetElement = document.querySelectorAll('[data-previous-opperand]')
 const currentOperandTexetElement = document.querySelectorAll('[data-current-opperand]')
+
+const Calculator = new Calculator(previousOperandTexetElement, currentOperandTexetElement),
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText) 
+        calculator.updateDisplay()
+    })
+})
