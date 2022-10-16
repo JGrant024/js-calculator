83b963708e05104676bae86d6ed443a088a842bf
JGrant024 <jonathan.grant23@icloud.com>
2022-10-16 18:22:05 -0400

    updated functionality

--- main.js
+++ main.js
@@ -1,48 +1,61 @@
-class Calculator {
-     constructor(previousOperandTexetElement, currentOperandTexetElement){
-        this.previousOperandTexetElement = previousOperandTexetElement
-        this,currentOperandTexetElement = currentOperandTexetElement
-        this.clear  
-     }
-}
-clear() {
+const numbers = document.querySelectorAll(".data-number");
+const operators = document.querySelectorAll(".data-operation");
+const output = document.querySelector(".previous-operand");
+const equal = document.querySelector(".data-equals");
+let clickedKey;
+let clickedOperator;
+let firstNumber = [];
+let secondNumber = [];
+let userInput = [];
+let calculation;
+let isOperatorClicked = true;
 
-}
-
-delete(){
-
-}
- 
-pushNumber(number) {
-
-}
-
-chooseOperation(operation) {
-
-}
-
-compute() {
-
-}
-
-updateDisplay() {
-    this.currentOperandTexetElement.innerText 
+function getNumber() {
+  numbers.forEach(function (number) {
+    number.addEventListener("click", (event) => {
+      clickedKey = event.target.value;
+      if (isOperatorClicked) {
+        firstNumber.push(clickedKey);
+        userInput.push(firstNumber);
+        output.innerHTML = firstNumber.join("");
+      } else {
+        secondNumber.push(clickedKey);
+        userInput.push(secondNumber);
+        output.innerHTML = secondNumber.join("");
+      }
+    });
+  });
 }
 
+function getOperator() {
+  operators.forEach(function (operator) {
+    operator.addEventListener("click", (event) => {
+      clickedOperator = event.target.value;
+      isOperatorClicked = false;
+      output.innerHTML = "";
+    });
+  });
+}
 
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButtons = document.querySelectorAll('[data-equals]')
-const deleteButton = document.querySelectorAll('[data-delete]')
-const allClearButton = document.querySelectorAll('[data-all-clear]')
-const previousOperandTexetElement = document.querySelectorAll('[data-previous-opperand]')
-const currentOperandTexetElement = document.querySelectorAll('[data-current-opperand]')
+equal.addEventListener("click", () => {
+  if (clickedOperator === "+") {
+    calculation =
+      parseInt(firstNumber.join("")) + parseInt(secondNumber.join(""));
+    output.innerHTML = calculation;
+  }
+  if (clickedOperator === "-") {
+    calculation = parseInt() - parseInt(secondNumber.join(""));
+    output.innerHTML = calculation;
+  }
+  if (clickedOperator === "*") {
+    calculation = parseInt() * parseInt(secondNumber.join(""));
+    output.innerHTML = calculation;
+  }
+  if (clickedOperator === "/") {
+    calculation = parseInt() / parseInt(secondNumber.join(""));
+    output.innerHTML = calculation;
+  }
+});
 
-const Calculator = new Calculator(previousOperandTexetElement, currentOperandTexetElement),
-
-$numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.pushNumber(button.innerText) 
-        calculator.updateDisplay()
-    })
-})
+getNumber();
+getOperator();
