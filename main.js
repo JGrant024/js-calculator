33e2e7e3cadc2cb5ccea133be1070e6ecbe2c1ee
JGrant024 <jonathan.grant23@icloud.com>
2022-10-16 21:42:00 -0400

    updated

--- main.js
+++ main.js
@@ -2,27 +2,24 @@ const numbers = document.querySelectorAll(".data-number");
 const operators = document.querySelectorAll(".data-operation");
 const output = document.querySelector(".previous-operand");
 const equal = document.querySelector(".data-equals");
+const clear = document.querySelector(".data-all-clear");
 let clickedKey;
 let clickedOperator;
-let firstNumber = [];
-let secondNumber = [];
+let firstNumber = "";
+let secondNumber = "";
 let userInput = [];
+let calcScreen = [];
 let calculation;
+let result;
 let isOperatorClicked = true;
 
 function getNumber() {
   numbers.forEach(function (number) {
     number.addEventListener("click", (event) => {
       clickedKey = event.target.value;
-      if (isOperatorClicked) {
-        firstNumber.push(clickedKey);
-        userInput.push(firstNumber);
-        output.innerHTML = firstNumber.join("");
-      } else {
-        secondNumber.push(clickedKey);
-        userInput.push(secondNumber);
-        output.innerHTML = secondNumber.join("");
-      }
+      userInput.push(clickedKey);
+      calcScreen.push(clickedKey);
+      output.innerHTML = calcScreen.join("");
     });
   });
 }
@@ -31,29 +28,61 @@ function getOperator() {
   operators.forEach(function (operator) {
     operator.addEventListener("click", (event) => {
       clickedOperator = event.target.value;
-      isOperatorClicked = false;
       output.innerHTML = "";
+      userInput.push(clickedOperator);
+      calcScreen = [];
     });
   });
 }
 
+clear.addEventListener("click", () => {
+  userInput = [];
+  calculation = "";
+  firstNumber = "";
+  secondNumber = "";
+  calcScreen = [];
+  output.innerHTML = "";
+});
+
 equal.addEventListener("click", () => {
-  if (clickedOperator === "+") {
-    calculation =
-      parseInt(firstNumber.join("")) + parseInt(secondNumber.join(""));
+  for (i = 0; i < userInput.length; i++) {
+    if (
+      userInput[i] === "+" ||
+      userInput[i] === "-" ||
+      userInput[i] === "/" ||
+      userInput[i] === "*"
+    ) {
+      var operator = userInput[i];
+    } else if (!operator) {
+      firstNumber = firstNumber + userInput[i];
+    } else if (operator) {
+      secondNumber = secondNumber + userInput[i];
+    }
+    calculate(firstNumber, operator, secondNumber);
+    console.log({ firstNumber });
+    console.log({ secondNumber });
+    console.log(userInput);
+
     output.innerHTML = calculation;
   }
-  if (clickedOperator === "-") {
-    calculation = parseInt() - parseInt(secondNumber.join(""));
-    output.innerHTML = calculation;
-  }
-  if (clickedOperator === "*") {
-    calculation = parseInt() * parseInt(secondNumber.join(""));
-    output.innerHTML = calculation;
-  }
-  if (clickedOperator === "/") {
-    calculation = parseInt() / parseInt(secondNumber.join(""));
-    output.innerHTML = calculation;
+
+  function calculate(firstNumber, operator, secondNumber) {
+    if (operator === "+") {
+      calculation = parseInt(firstNumber) + parseInt(secondNumber);
+      firstNumber = "";
+    }
+    if (operator === "-") {
+      calculation = parseInt(firstNumber) - parseInt(secondNumber);
+      output.innerHTML = calculation;
+    }
+    if (operator === "*") {
+      calculation = parseInt(firstNumber) * parseInt(secondNumber);
+      output.innerHTML = calculation;
+    }
+    if (operator === "/") {
+      calculation = parseInt(firstNumber) / parseInt(secondNumber);
+      output.innerHTML = calculation;
+    }
   }
 });
 
